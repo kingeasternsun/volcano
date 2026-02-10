86cfb6f0df572744bbf0fa13b9634f3e1da68644
kingeasternsun <kingeasternsun@gmail.com>
2026-02-10 11:25:26 +0800

    fix mpi job plugin
    Signed-off-by: kingeasternsun <kingeasternsun@gmail.com>

--- pkg/webhooks/admission/jobs/validate/admit_job.go
+++ pkg/webhooks/admission/jobs/validate/admit_job.go
@@ -145,14 +145,9 @@ func validateJobCreate(job *v1alpha1.Job, reviewResponse *admissionv1.AdmissionR
 	if _, ok := job.Spec.Plugins[controllerMpi.MPIPluginName]; ok {
 		mp := controllerMpi.NewInstance(job.Spec.Plugins[controllerMpi.MPIPluginName])
 		masterIndex := helpers.GetTaskIndexUnderJob(mp.GetMasterName(), job)
-		workerIndex := helpers.GetTaskIndexUnderJob(mp.GetWorkerName(), job)
 		if masterIndex == -1 {
 			reviewResponse.Allowed = false
 			return "The specified mpi master task was not found"
-		}
-		if workerIndex == -1 {
-			reviewResponse.Allowed = false
-			return "The specified mpi worker task was not found"
 		}
 	}
 
